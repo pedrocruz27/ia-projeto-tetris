14d222f28b7cd3ca2a5bab4b7dcc5124c0eaf077
touret <miguel_touret@hotmail.com>
2015-11-03 22:48:50 +0000

    more stuff

--- project.lisp
+++ project.lisp
@@ -10,5 +10,24 @@
   )
 
 (defun tabuleiro-preenchido-p (tabuleiro l c)
-  (aref tabuleiro l c)
+  (aref tabuleiro l-1 c-1)
   )
+
+(defun tabuleiro-altura-coluna (tabuleiro c)
+  (setf resultado 0)
+  (setf l (1- (first (array-dimensions tabuleiro))))
+  (dotimes (i l resultado) 
+    (if (tabuleiro-preenchido-p tabuleiro l c)
+        (+1 l)
+      )
+    )
+  )
+
+(defun tabuleiro-linha-completa-p (tabuleiro l)
+  (if (< l-1 0) 
+      (cond ((tabuleiro-preenchido-p (tabuleiro l c)))
+            
+            
+            
+      )
+    )
